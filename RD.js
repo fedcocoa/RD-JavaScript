90a3bbf810419e3dc3f133c359e2c1ccb1fe0fc5
Rohan Dewan <35611893+rohandewan@users.noreply.github.com>
2018-01-20 12:32:57 +0000

    New classes
    Added objects list and classes for points and lines. Function for setting size of canvas and various other drawing functions.

--- RD.js
+++ RD.js
@@ -1,15 +1,29 @@
-console.log("RD.js loaded.");
+//This file is the main RD.js Library. It contains all the basics for the Library.
+//At the time of writing this I haven't got any other files for other functionality but
+//I plan to add more files later down the line but for now, I'm going to focus on this one.
+//Please don't edit stuff in here unless you know what you are doing, changes could break
+//functionality.
+
+//Rohan Dewan
+
+console.log("RD.js Library");
 
 objects = [];
 
 setInterval(draw,60);
 
-class Circle {
-  constructor(x,y,r,c) {
+class Point {
+  constructor(x,y) {
     this.x = x || 0;
     this.y = y || 0;
-    this.r = r || 10;
-    this.c = c || "black";
+  }
+}
+
+class Circle {
+  constructor(x,y,r,c) {
+    this.center = new Point(x,y);
+    this.radius = r || 10;
+    this.colour = c || "white";
     objects.push(this);
   }
   draw(){
@@ -17,16 +31,38 @@ class Circle {
   }
 }
 
-function canvasSize(width,height) {
+class Line {
+  constructor(x1,y1,x2,y2,w,c) {
+    this.start = new Point(x1 || 0,y1 || 0);
+    this.end = new Point(x2 || 0, y2 || 0);
+    this.width = w || 1;
+    this.colour = c || "white";
+    objects.push(this);
+  }
+  draw() {
+    drawLine(this);
+  }
+}
+
+function setSize(width,height) {
   canvas.width = width;
   canvas.height = height;
 }
 
 function drawCircle(circle) {
+  ctx.fillStyle = circle.colour;
   ctx.beginPath();
-  ctx.arc(circle.x,circle.y,circle.r,0,Math.PI*2);
-  ctx.fillStyle = circle.c;
+  ctx.arc(circle.center.x,circle.center.y,circle.radius,0,Math.PI*2);
   ctx.fill();
+}
+
+function drawLine(line) {
+  ctx.lineWidth = line.width;
+  ctx.strokeStyle = line.colour;
+  ctx.beginPath();
+  ctx.moveTo(line.start.x,line.start.y);
+  ctx.lineTo(line.end.x,line.end.y);
+  ctx.stroke();
 }
 
 function clearCanvas() {
@@ -35,6 +71,10 @@ function clearCanvas() {
 
 function draw() {
   clearCanvas();
+  drawObjects();
+}
+
+function drawObjects() {
   for(var i = 0; i < objects.length; i++) {
     objects[i].draw();
   }
